fdda52066d1e68e30a2e47124716d56a5e649d2b
Rafael13 <rafaeldepaula1325@gmail.com>
2023-01-27 18:47:56 -0300

    creat desing form login

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+import LoginPage from './pages/LoginPages';
+import HomePage from './HomePage/index';
+
+function App() {
+  return (
+    <div className="app">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
